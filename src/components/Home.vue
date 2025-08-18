94fe3d9fae0a8001ba171ede05caf5a77a7dfd12
lbjcena <lbjcena@gmail.com>
2025-08-19 01:07:52 +0200

    desktop menu almost done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,8 @@
-<script setup lang="ts">
-
-
-
-
-</script>
 <template>
-  <main class="flex flex-col items-center flex-1 h-[calc(100%-96px)]">
-    <section class="text flex flex-col flex-1 h-1/2 items-center tablet:justify-center gap-300 p-300 tablet:p-1600">
-      <h1 class="flex flex-col text-center">
+  <main class="flex flex-col desktop:flex-row items-center flex-1 h-[calc(100%-96px)] desktop:items-end desktop:p-1600">
+    <section
+      class="text desktop:relative flex flex-col flex-1 h-1/2 items-center tablet:justify-center gap-300 p-300 tablet:p-1600 desktop:bottom-1600">
+      <h1 class="flex flex-col text-center desktop:w-2/3 desktop:text-left desktop:gap-400">
         <span class="text-light text-preset-5 block uppercase">So, you want to travel to</span>
         <span class="text-preset-1 block text-white">SPACE</span>
         <span class="text-preset-9 text-light block">Let’s face it; if you want to go to space, you might as well
@@ -15,11 +10,32 @@
           give you a truly out of this world experience!</span>
       </h1>
     </section>
-    <section class="button flex-1 h-1/2 w-full flex justify-center items-center tablet:p-600">
+    <section
+      class="button desktop:relative flex-1 h-1/2 w-full flex justify-center items-center desktop:justify-end tablet:p-600 desktop:bottom-1600">
       <router-link to="/destination"
-        class="bg-white text-dark text-preset-4 rounded-[50%] aspect-square h-[144px] w-[144px] tablet:h-[250px] tablet:w-[250px] uppercase text-center flex items-center justify-center">Explore</router-link>
+        class="btn-explore bg-white text-dark relative text-preset-4 rounded-[50%] p-[88px] tablet:h-[250px] tablet:w-[250px] uppercase text-center flex items-center justify-center">Explore</router-link>
     </section>
-
   </main>
 </template>
-<style scoped></style>
+<style scoped>
+.btn-explore::after {
+  content: '';
+  position: absolute;
+  inset: 0;
+  /* <-- rozciąga pseudo na całość przycisku */
+  border-radius: 50%;
+  background-color: transparent;
+  /* default, żeby nie przykrywał */
+  transition: transform 0.5s ease, background-color 0.3s ease;
+  z-index: -1;
+  /* <-- daje go pod spód */
+}
+.btn-explore:hover::after {
+  transform: scale(1.75);
+  /* zamiast height/width bawisz się skalą */
+  background-color: rgba(255, 255, 255, 0.3);
+}
+/* .btn-explore:hover {
+  border: 40px solid lightgray;
+} */
+</style>
