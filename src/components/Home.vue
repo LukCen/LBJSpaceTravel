334fe691c98529b1bb5d45605aa7e7ff9ccd75c9
lbjcena <lbjcena@gmail.com>
2025-08-18 16:12:47 +0200

    css optimizations to typography, tablet and mobile variants added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,14 @@
 
 </script>
 <template>
-  <main>
+  <main class="flex flex-col items-center ">
+    <section class="text flex flex-col items-center gap-300">
+      <h1 class="flex flex-col text-center">
+        <span class="text-light text-preset-6 block">So, you want to travel to</span>
+        <span class="text-preset-1 block text-white">SPACE</span>
+        <span class="text-preset-9 text-light block"></span>
+      </h1>
+    </section>
 
   </main>
 </template>
