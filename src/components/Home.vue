c89f155c0f8262044cb693cd418b44a4893bc31a
lbjcena <lbjcena@gmail.com>
2025-08-18 23:22:44 +0200

    tablet version of homepage added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,19 +5,19 @@
 
 </script>
 <template>
-  <main class="flex flex-col items-center h-full">
-    <section class="text flex flex-col items-center gap-300 p-300">
+  <main class="flex flex-col items-center flex-1 h-[calc(100%-96px)]">
+    <section class="text flex flex-col flex-1 h-1/2 items-center tablet:justify-center gap-300 p-300 tablet:p-1600">
       <h1 class="flex flex-col text-center">
-        <span class="text-light text-preset-6 text-preset-5 block uppercase">So, you want to travel to</span>
+        <span class="text-light text-preset-5 block uppercase">So, you want to travel to</span>
         <span class="text-preset-1 block text-white">SPACE</span>
         <span class="text-preset-9 text-light block">Let’s face it; if you want to go to space, you might as well
           genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll
           give you a truly out of this world experience!</span>
       </h1>
     </section>
-    <section class="button h-1/2 flex justify-center items-center">
+    <section class="button flex-1 h-1/2 w-full flex justify-center items-center tablet:p-600">
       <router-link to="/destination"
-        class="bg-white text-dark text-preset-4 rounded-[50%] aspect-square h-[144px] w-[144px] uppercase text-center flex items-center justify-center">Explore</router-link>
+        class="bg-white text-dark text-preset-4 rounded-[50%] aspect-square h-[144px] w-[144px] tablet:h-[250px] tablet:w-[250px] uppercase text-center flex items-center justify-center">Explore</router-link>
     </section>
 
   </main>
