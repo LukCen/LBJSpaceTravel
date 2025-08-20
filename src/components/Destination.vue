461c049ffa3d27da2d7fd1c5ebd562bd5b369e60
lbjcena <lbjcena@gmail.com>
2025-08-20 16:17:31 +0200

    destination view mvp finished

--- src/components/Destination.vue
+++ src/components/Destination.vue
@@ -42,29 +42,36 @@ const activeId = ref(0)
 
 </script>
 <template>
-  <main class="destination relative flex flex-col h-[calc(100%-96px)] w-full gap-300 p-600">
-    <h1 class="text-preset-6 uppercase text-white text-center desktop:text-left">01 Pick your destination</h1>
-    <section class="content flex flex-col desktop:flex-row items-center gap-300 text-white">
-      <img :src="data[activeId].img" alt="" class="scale-75 desktop:scale-100">
-      <ul class="flex gap-300">
-        <li v-for="item in data"><button :id="item.id" @click="activeId = item.id"
-            :class="{ isActive: activeId === item.id }"
-            class="w-full text-light uppercase text-preset-8 cursor-pointer border-b-2 border-transparent p-1">{{
-              item.planet
-            }}</button>
-        </li>
-      </ul>
-      <div class="text w-full desktop:w-1/2">
+  <main
+    class="destination relative flex flex-col h-[calc(100%-96px)] w-full gap-300 p-600 tablet:p-1200 desktop:p-1200 desktop:items-center desktop:justify-center">
+    <h1 class="text-preset-5 uppercase text-white text-center desktop:text-left"><span
+        class="text-gray-300 font-bold">01
+      </span>Pick your destination</h1>
+    <section
+      class="content flex flex-col desktop:flex-row items-center desktop:justify-center gap-300 text-white desktop:gap-1600 desktop:min-h-[500px] desktop:min-w-[1600px]">
+      <div class="img flex">
+        <img :src="data[activeId].img" alt="" class="scale-75 desktop:scale-100">
+
+      </div>
+      <div class="text w-full desktop:max-w-[500px] text-center desktop:text-left">
+        <ul class="flex justify-center gap-300 desktop:justify-start">
+          <li v-for="item in data"><button :id="item.id" @click="activeId = item.id"
+              :class="{ isActive: activeId === item.id }"
+              class="w-full text-light uppercase text-preset-8 cursor-pointer pb-100 border-b-2 border-transparent p-1">{{
+                item.planet
+              }}</button>
+          </li>
+        </ul>
         <h2 class="text-preset-2 uppercase">{{ data[activeId].planet }}</h2>
         <p class="text-preset-9">{{ data[activeId].desc }}</p>
-        <hr>
-        <div class="flex gap-600">
-          <div class="flex flex-col gap-300">
-            <span class="text-preset-7 uppercase">Avg. Distance</span>
+        <hr class="my-8 border-light">
+        <div class="flex gap-400 flex-col tablet:flex-row desktop:h-full desktop:flex-row">
+          <div class="flex flex-1/2 flex-col gap-300">
+            <span class="text-preset-7 uppercase text-light">Avg. Distance</span>
             <span class="text-preset-6 uppercase">{{ data[activeId].distance }}</span>
           </div>
-          <div class="flex flex-col gap-300">
-            <span class="text-preset-7 uppercase">Est. Travel Time</span>
+          <div class="flex flex-1/2 flex-col gap-300">
+            <span class="text-preset-7 uppercase text-light">Est. Travel Time</span>
             <span class="text-preset-6 uppercase">{{ data[activeId].travel_time }}</span>
           </div>
         </div>
